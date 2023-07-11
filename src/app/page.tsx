bc3ec1e088850ab3304b12de92984ca4555891fa
marcoD3V3 <git.sinister.developer@gmail.com>
2023-07-11 18:43:42 -0500

    hola soli 11-07-23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./page.module.css";
+import styles from "./pageH.module";
 import ScrollToTop from "react-scroll-up";
 import { Problem } from "@/components";
 
